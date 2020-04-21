69b738ba22e13a7c2bf16129a1e2e877fc873d29
CobyBoy <aboutacoby@gmail.com>
2020-04-20 22:37:46 -0300

    cambio de orden e if por switch

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,25 +5,34 @@ public class Calculadora {
 		Integer b = 0;
 		Integer opcion;
 		Scanner teclado = new Scanner(System.in);
-		Integer resultado;
+		Integer resultado = null;
 		do {
 			System.out.println(
-					"Ingrese su operación /1 para sumar /2 para restar /n 3 para multiplicar /n 4 para dividir");
+					"Ingrese su operación \n 1 para multiplicar \n 2 para restar \n 3 para sumar \n 4 para dividir");
 			opcion = teclado.nextInt();
 		} while (opcion < 1 && opcion > 4);
+		
 		System.out.println("Ingrese el primer numero");
 		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
 		b = teclado.nextInt();
-		if (opcion.equals(3)) {
-			resultado = a + b;
-		} else if (opcion.equals(2)) {
-			resultado = a - b;
-		} else if (opcion.equals(1)) {
-			resultado = a * b;
-		} else {
-			resultado = a / b;
+		
+		switch (opcion) {
+		case 3:
+			resultado = a+b;
+			break;
+		case 2:
+			resultado = a-b;
+			break;
+		case 1:
+			resultado = a*b;
+		case 4:
+		resultado = a/b ;
+			break;
+		default: 
+			break;
 		}
+		
 		System.out.println("El resultado es " + resultado.toString());
 	}
 }
